a82728534d1b1cec8ea99abfb8c9f583fe196b14
dante canizo <dantecanizo@dantes-MacBook-Pro-2.local>
2024-12-10 18:48:45 -0300

    Adding base project

---
+++ Discogsmusicfinder/DiscogsmusicfinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscogsmusicfinderApp.swift
+//  Discogsmusicfinder
+//
+//  Created by dante canizo on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiscogsmusicfinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
